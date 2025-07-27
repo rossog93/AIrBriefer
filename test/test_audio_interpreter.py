c470d79cc725f27283fa843af3dfb7dc57d39a0a
Gabriel Rosso <rosso.gabriel1@gmail.com>
2025-07-27 00:29:55 -0300

    Add code to validate the AI code that translate and the DSP on the captured signal

--- test/test_audio_interpreter.py
+++ test/test_audio_interpreter.py
@@ -1,8 +1,11 @@
 import os
 from src.ai_processor import AITranscriber
 
+from src.utils.logger import logger
+logger.info("initializing AITranscriber")
 
 audio_file = os.path.join(os.getcwd(), 'test', 'audio_test.ogg')
-transcriber = AITranscriber(model_name="base")  # Use your desired Whisper model
-transcription = transcriber.transcribe(audio_file)
+transcriber = AITranscriber(model_name="large")  # Use your desired Whisper model
+transcription = transcriber.transcribe(audio_file, reduce_noise=True) #add a reduce noise flag
+
 print(transcription)
